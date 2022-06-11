8b1fb870b72566719fa2bc61f9131d9c8cdde4b1
Berruz999 <bexruzfatullayev999@gmail.com>
2022-06-11 15:16:34 +0500

    messenger 1.0

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,10 +35,13 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => false,
-            ],
+	        'options' => [
+		        'cluster' => env('PUSHER_APP_CLUSTER'),
+		        'encrypted' => false,
+		        'host' => '127.0.0.1',
+		        'port' => 6001,
+		        'scheme' => 'http'
+	        ],
         ],
 
         'ably' => [
